0aa4a4c5c5bdbad5d443b455db8a2a1c9cd249e5
David-Raffo <marconidavid@hotmail.es>
2025-03-19 00:28:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=os.getenv("PORT", default=4000))
+    port = int(os.environ.get("PORT", 4000))  # Forzar conversión a entero
+    app.run(debug=False, host="0.0.0.0", port=port)
+
 
 
 
