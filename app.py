4fcdd4a81d734976dcdbfbbc69e900da2cc52336
David-Raffo <marconidavid@hotmail.es>
2025-03-19 00:16:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0", port=os.getenv("PORT", default=4000))
 
 
 
